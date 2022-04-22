7fb77928ebc5d0d48225b9c5a46baba98f703478
CChenLi <lichen918@g.ucla.edu>
2022-04-22 12:38:49 -0400

    Add audio play, keydown play audio interaction

--- Source/static/clef.js
+++ Source/static/clef.js
@@ -1,5 +1,46 @@
+var recent_key = "Q"
+
+function play_key(key) {
+    var audio_path = lesson.audio_path[key]
+    const audio = new Audio(audio_path);
+    audio.play();
+    recent_key = key
+    $("#players").empty()
+    load_player()
+}
+
+function load_key() {
+    $(document).keydown(function (e) {
+        var key = e.key.toUpperCase();
+        if (lesson.audio_order.includes(key)) {
+            play_key(key)
+        }
+    })
+}
+
+function load_player() {
+    var player_pad = $("<div id='player-pad'>")
+    $("#players").append(player_pad)
+    lesson.audio_order.forEach(element => {
+        var player = $("<div class='player'>")
+        console.log(recent_key)
+        player.click(function() {
+            play_key(element)
+        });
+        if (element == recent_key) {
+            var play_button = $("<img src='/static/images/play-button2.png' class='lesson-img'>")
+        } else {
+            var play_button = $("<img src='/static/images/play-button.png' class='lesson-img'>")
+        }
+        player.append(play_button)
+        $("#players").append(player)
+    });
+}
+
 $(document).ready(function(){
     id= parseInt(id)
+    load_player()
+    load_key()
     if (id > 1){
         var prev = $('<button type = "button" class = "btn btn-light rightmargin" id = "prev"> Prev </button>')
         $(prev).on( "click", function(event) {
